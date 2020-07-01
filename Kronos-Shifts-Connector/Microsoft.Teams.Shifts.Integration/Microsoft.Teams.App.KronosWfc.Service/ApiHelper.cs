67aab560223efb61f5b67f87a4d82cdd40d1d953
CMendez0 <67651601+CMendez0@users.noreply.github.com>
2020-07-01 10:44:18 -0400

    Update ApiHelper.cs

--- Kronos-Shifts-Connector/Microsoft.Teams.Shifts.Integration/Microsoft.Teams.App.KronosWfc.Service/ApiHelper.cs
+++ Kronos-Shifts-Connector/Microsoft.Teams.Shifts.Integration/Microsoft.Teams.App.KronosWfc.Service/ApiHelper.cs
@@ -59,7 +59,7 @@ namespace Microsoft.Teams.App.KronosWfc.Service
             var request = new RestRequest(Method.POST);
             request.AddHeader("Authorization", "Basic Uko4OWR4dXVHODdKT3dBV3JyaGtQR1hKQVVQcmp0Sjk6aFI1ZlJjWkxjZUo2aWw2UQ==");
             request.AddParameter("text/plain", "", ParameterType.RequestBody);
-            //IRestResponse response = client.Execute(request);
+            IRestResponse response = client.Execute(request);
 
             // Console.WriteLine(response.Content);
 
