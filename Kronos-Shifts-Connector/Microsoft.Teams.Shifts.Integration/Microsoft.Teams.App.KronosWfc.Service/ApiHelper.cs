3fa86aabc2c777bdf357d47aad3ebdee4f8fe38d
CMendez0 <67651601+CMendez0@users.noreply.github.com>
2020-07-01 13:51:21 -0400

    Update ApiHelper.cs

--- Kronos-Shifts-Connector/Microsoft.Teams.Shifts.Integration/Microsoft.Teams.App.KronosWfc.Service/ApiHelper.cs
+++ Kronos-Shifts-Connector/Microsoft.Teams.Shifts.Integration/Microsoft.Teams.App.KronosWfc.Service/ApiHelper.cs
@@ -52,23 +52,23 @@ namespace Microsoft.Teams.App.KronosWfc.Service
         }
 
 
-        private string AuthenticateToken()
-        {
-            var client = new RestClient("https://dev.api.tjx.com/gies/v1/oauth2/accesstoken?grant_type=client_credentials");
-            client.Timeout = -1;
-            var request = new RestRequest(Method.POST);
-            request.AddHeader("Authorization", "Basic Uko4OWR4dXVHODdKT3dBV3JyaGtQR1hKQVVQcmp0Sjk6aFI1ZlJjWkxjZUo2aWw2UQ==");
-            request.AddParameter("text/plain", "", ParameterType.RequestBody);
-            IRestResponse response = client.Execute(request);
+        //private string AuthenticateToken()
+        //{
+            //var client = new RestClient("https://dev.api.tjx.com/gies/v1/oauth2/accesstoken?grant_type=client_credentials");
+            //client.Timeout = -1;
+            //var request = new RestRequest(Method.POST);
+            //request.AddHeader("Authorization", "Basic Uko4OWR4dXVHODdKT3dBV3JyaGtQR1hKQVVQcmp0Sjk6aFI1ZlJjWkxjZUo2aWw2UQ==");
+            //request.AddParameter("text/plain", "", ParameterType.RequestBody);
+            //IRestResponse response = client.Execute(request);
 
             // Console.WriteLine(response.Content);
 
-            string source = response.Content;
-            dynamic data = JObject.Parse(source);
+            //string source = response.Content;
+            //dynamic data = JObject.Parse(source);
             //string accessToken = data.access_token;
-            string accessToken = "QxsFyrADoTVZBtA8AD5tUIhcGA3k";
-            return accessToken;
-        }
+            //string accessToken = "QxsFyrADoTVZBtA8AD5tUIhcGA3k";
+          //  return accessToken;
+        //}
 
         /// <summary>
         /// Post XMl request.
@@ -79,13 +79,13 @@ namespace Microsoft.Teams.App.KronosWfc.Service
         /// <returns>Response message.</returns>
         private async Task<HttpResponseMessage> PostXmlRequestAsync(Uri baseUrl, string xmlString, string jSession)
         {
-            var accessToken = this.AuthenticateToken();
+            //var accessToken = this.AuthenticateToken();
             if (string.IsNullOrEmpty(jSession))
             {
                 using (var httpClient = new HttpClient())
                 {
                     
-                    httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+                    httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", "FGWwop1SwAQ7z9eEQjou4wcZGxoY");
                     using (var httpContent = new StringContent(xmlString, Encoding.UTF8, "text/xml"))
                     {
                         httpContent.Headers.Add("SOAPAction", ApiConstants.SoapAction);
@@ -99,7 +99,7 @@ namespace Microsoft.Teams.App.KronosWfc.Service
                 {
                     using (var httpClient = new HttpClient(httpClientHandler))
                     {
-                        httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+                        httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", "FGWwop1SwAQ7z9eEQjou4wcZGxoY");
                         using (var httpContent = new StringContent(xmlString, Encoding.UTF8, "text/xml"))
                         {
                             httpContent.Headers.Add("SOAPAction", ApiConstants.SoapAction);
